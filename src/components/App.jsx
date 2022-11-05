f31a1ff619e7ea84b8da5a6112076781181da41d
arseniy-grinev <arseniy.grinev@gmail.com>
2022-11-05 17:18:42 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,87 +1,24 @@
-import { useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import * as API from '../services/api';
 
-import Searchbar from './Searchbar';
-import ImageGallery from './ImageGallery';
-import Loader from './Loader';
-import Button from './Button';
-import Modal from './Modal';
+import { Route, Routes } from "react-router-dom";
+import SharedLayout from "./SharedLayout";
 
-function App() {
-  const [searchImage, setSearchImage] = useState('');
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [status, setStatus] = useState('idle');
-  const [showModal, setShowModal] = useState(false);
-  const [largeImageModal, setLargeImageModal] = useState(null);
-  // state = {
-  //   searchImage: '',
-  //   images: [],
-  //   page: 1,
-  //   status: 'idle',
-  //   showModal: false,
-  //   largeImageModal: null,
-  // };
 
-  useEffect(() => {
-    if (!searchImage) {
-      return;
-    }
 
-    async function render() {
-      setStatus('pending');
-
-      try {
-        const imageList = await API.getImages(searchImage, page);
-        setImages([...images, ...imageList]);
-        setStatus('resolved');
-
-        if (imageList.length === 0) {
-          toast.error(
-            'Sorry, there are no images matching your search query. Please, try again.'
-          );
-        }
-      } catch (error) {
-        toast.error('Something went wrong. Please, reload the page.');
-        setStatus('rejected');
-      }
-    }
-    render();
-  }, [images, page, searchImage]);
-
-  const onFormSubmit = searchImage => {
-    setSearchImage(searchImage);
-    setImages([]);
-    setPage(1);
-  };
-
-  const onLoadBtnClick = () => {
-    setPage(prevPage => prevPage + 1);
-  };
-
-  const onToggleModal = largeImageURL => {
-    setShowModal(!showModal);
-    setLargeImageModal(largeImageURL);
-  };
-
+ const App = () => {
   return (
-    <>
-      <Searchbar onSubmit={onFormSubmit} />
-      {images.length > 0 && (
-        <ImageGallery items={images} onClick={onToggleModal} />
-      )}
-      {status === 'pending' && <Loader />}
-      {images.length >= 12 && <Button onClick={onLoadBtnClick} />}
-      {showModal && (
-        <Modal onClose={onToggleModal}>
-          <img src={largeImageModal} alt="" />
-        </Modal>
-      )}
-      <ToastContainer autoClose={3000} position="top-right" />
-    </>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<div>Home</div>} />
+        <Route path="about" element={<div>About</div>}>
+          <Route path="mission" element={<div>Mission</div>} />
+          <Route path="team" element={<div>Team</div>} />
+          <Route path="reviews" element={<div>Reviews</div>} />
+        </Route>
+        <Route path="products" element={<div>Products</div>} />
+        <Route path="products/:id" element={<div>ProductsId</div>} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
